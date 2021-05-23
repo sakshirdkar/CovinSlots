a4f7a057faa9cd432abba778108429d274b09dec
unknown <sakshi.rdkar@gmail.com>
2021-05-23 17:35:27 +0530

    Second commit

--- controller.js
+++ controller.js
@@ -12,7 +12,7 @@ const API_Call = async function (day) {
     let session = [];
 
     request(process.env.API_URL + day, options, (error, res, body) => {
-      if (error) throw new Error(`Error ocurred(${res.status})`);
+      if (error) throw error;
       if (!error && res.statusCode == 200) {
         session = body.sessions;
       }
@@ -28,13 +28,14 @@ const fetchData = async function () {
   try {
     const Today = new Date();
     let day = new Date();
-    day.setDate(Today.getDate() + 7);
+    day.setDate(Today.getDate() + 2);
 
     while (day.getTime() >= Today.getTime()) {
-      const session = await API_Call(day);
-
+      let day_ = `${day.getDate()}-${day.getMonth() + 1}-${day.getFullYear()}`;
+      const session = await API_Call(day_);
+      console.log("From fetch", session, day_);
       if (session.length != 0) {
-        if (session.min_age_limit == 18) VaccineAppointments.push(session);
+        if (session.min_age_limit == 45) VaccineAppointments.push(session);
       }
 
       day.setDate(day.getDate() - 1);
